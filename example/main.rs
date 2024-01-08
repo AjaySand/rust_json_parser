3134851802405012db302e680ccd790e4b7b1b16
Ajaydeep Sandhu <ajaysan.1996@gmail.com>
2024-01-08 16:13:25 +0100

    testing big file

--- example/main.rs
+++ example/main.rs
@@ -1,7 +1,8 @@
 use rust_json_parser::{parse, tokenizer::tokenize_json};
 
 fn main() {
-    let input = include_str!("../example/inputs/test_1.json");
+    // let input = include_str!("../example/inputs/test_1.json");
+    let input = include_str!("../example/inputs/large_placeholder.json");
 
     let obj = parse(input);
     println!("{:#?}", obj);
